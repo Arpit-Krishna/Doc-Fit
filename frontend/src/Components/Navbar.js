367a63ca0765107e4e4623691e73b78f6b0e9a5b
agatya mishra <agatyamishra@gmail.com>
2024-05-08 22:19:20 +0530

    logo_update

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import Icon from  './assets/icon.png';
+import './style/Navbar.css'
 
 export default function Navbar() {
     return (
@@ -6,7 +8,7 @@ export default function Navbar() {
             <div className='flex justify-between p-7'>
                 <div>
                     <div className='text-xl font-semibold'>
-                        Website Logo
+                        <img src={Icon} alt='logo' className='logo -my-1' />
                     </div>
                 </div>
                 <div className='flex w-2/3 justify-evenly'>
@@ -30,8 +32,8 @@ export default function Navbar() {
                             ABOUT US
                         </a>
                     </div>
-                    <div className='text-lg font-semibold bg-clip-text text-transparent bg-blue-950 border px-4 rounded-xl border-blue-900 hover:bg-blue-700 hover:text-white hover:bg-clip-padding '>
-                        <button>
+                    <div className=' h-12 text-lg font-semibold bg-clip-text text-transparent -my-3 bg-blue-950 border px-4 rounded-xl border-blue-900 hover:bg-blue-700 hover:text-white hover:bg-clip-padding '>
+                        <button className='my-2'>
                             LOGIN
                         </button>
                     </div>
